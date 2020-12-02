8a3456dcbe8656e9fc6071e261a262911ac4c7d6
amrit <amrit_padda@live.ca>
2020-12-02 16:32:43 -0500

    WIP loader

--- loader/packet.py
+++ loader/packet.py
@@ -1,17 +1,21 @@
 class packet():
-    size = 0
-    checksum = 0
-    data = 0
     commands = {'ping': 32, 'getstatus': 35, 'download': 33, 'sendData': 36, 'run': 34, 'reset': 37}
     packet = []
 
     def __init__(self, command):
-        #for now this only supports 1 command at a time
-        self.data = self.commands[command]
-        self.checksum = self.data
-        self.size = 3   #hardcoded for now
+        comm = command.split()
+        data = []
 
-        self.packet = [self.size, self.checksum, self.data, 0]
+        if comm[0] == 'download':
+            address = comm[1]
+            program_size = comm[2]
+            data.append(commands[comm[0]])
+            data.append(commands[comm[1]])
+
+        checksum = sum(data) #has to be truncated
+        size = len(data)  
+
+        self.packet = [size, checksum, data, 0]
 
     def get_packet(self):
         return bytearray(self.packet)
