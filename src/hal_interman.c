de2be774b630a5f7fdd0879f6a13395aca1b8479
amrit <amrit_padda@live.ca>
2020-12-04 19:40:42 -0500

    adding skeleton files for task 6

---
+++ src/hal_interman.c
@@ -0,0 +1,22 @@
+
+#include "hal.h"
+
+
+void hal_Interrupt_Disable(void){
+
+}
+void hal_Interrupt_Enable(void){
+
+}
+u16 hal_Interrupt_SaveAndDisable(void){
+
+}
+void hal_Interrupt_Restore(u16 flags){
+
+}
+void hal_Interrupt_SetVector(u8 number, u32 handlerAddr){
+
+}
+u32 hal_Interrupt_GetVector(u8 number){
+    
+}
---
+++ src/hal_interman.h
@@ -0,0 +1,14 @@
+// hal_interman.h -- Interrupt Management Interface
+#ifndef __hal_Interrupt_h
+#define __hal_Interrupt_h
+
+#include "_stdtype.h"
+
+void hal_Interrupt_Disable(void);
+void hal_Interrupt_Enable(void);
+u16 hal_Interrupt_SaveAndDisable(void);
+void hal_Interrupt_Restore(u16 flags);
+void hal_Interrupt_SetVector(u8 number, u32 handlerAddr);
+u32 hal_Interrupt_GetVector(u8 number);
+
+#endif
