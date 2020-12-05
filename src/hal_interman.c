ee10ec7ddfd41496b32da042880283c822a58952
amrit <amrit_padda@live.ca>
2020-12-04 19:56:30 -0500

    Added some code for task 6

--- src/hal_interman.c
+++ src/hal_interman.c
@@ -1,11 +1,22 @@
 
 #include "hal.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
 
 
 void hal_Interrupt_Disable(void){
-
+    cli();
 }
 void hal_Interrupt_Enable(void){
+    DDRD &= ~(1 << DDD2);     // Clear PD2 
+
+    PORTD |= (1 << PORTD2);    // set PD2 as Pull-up
+    
+    EICRA |= (1 << ISC00);    // set INT0 to trigger on ANY logic change
+    EIMSK |= (1 << INT0);     // Turns on INT0
+
+    sei(); 
 
 }
 u16 hal_Interrupt_SaveAndDisable(void){
@@ -14,9 +25,4 @@ u16 hal_Interrupt_SaveAndDisable(void){
 void hal_Interrupt_Restore(u16 flags){
 
 }
-void hal_Interrupt_SetVector(u8 number, u32 handlerAddr){
 
-}
-u32 hal_Interrupt_GetVector(u8 number){
-    
-}
